c092c47d2f1cdcee7fe5e4129bd38006e17eb06f
Brendo Costa <brendocosta@id.uff.br>
2021-11-01 23:26:09 -0300

    First release

--- src/include/mode_seven.lua
+++ src/include/mode_seven.lua
@@ -4,7 +4,7 @@
   @ Path              /src/include/
   @ Author            Brendo Costa <brendocosta@id.uff.br>
   @ Description       Nintendo SNES's "mode seven" Love2d implementation
-  @ Updated           Oct 27, 2021
+  @ Updated           Nov 1, 2021
 
   ------------------------------------------------------------------------------
 
@@ -36,361 +36,244 @@
 
 local self = {
 
+    enableGroundLimit = true,
+
     -- This instance's camera
 
     camera = {
-
         position  = { x = 0, y = 0, z = 10 },
-        width     = love.graphics.getWidth(),
-        height    = love.graphics.getHeight(),
-        horizon   = (love.graphics.getHeight() / 2),
+        screen    = {
+            width     = love.graphics.getWidth(),
+            height    = love.graphics.getHeight(),
+            horizon   = (love.graphics.getHeight() / 2),
+            scanLines = 1
+        },
         rotation  = 0,
-        zoom      = 1,
-        fov       = (love.graphics.getHeight() / 2),
         pitch     = 0,
-        scanLines = 1
-
     },
 
+    -- This instance's map
+
     map = {
-
         position = { x = 0, y = 0 },
-        width    = 512,
-        length   = 512,
-
-        image    = {
-
+        width,
+        length,
+        image = {
             data,
             width,
             height,
-            scale = { x = 1, y = 1 }
-
+            scale = { x, y }
         }
-
-    },
-
-    enableGroundLimit = true
+    }
 
 }
 
-function self.init(width, height, mapWidth, mapLength, imageData)
+function self.init(args)
 
-    self.camera.width     = width
-    self.camera.height    = height
-    self.camera.horizon   = height / 2
+    --[[
 
-    self.map.width        = mapWidth
-    self.map.length       = mapLength
-    self.map.image.data   = imageData
-    self.map.image.width  = imageData:getWidth() - 1
-    self.map.image.height = imageData:getHeight() - 1
+        Possible arguments:
+
+        - Screen   : screenWidth, screenHeight, screenHorizon
+        - Map      : mapImageData, mapWidth, mapLength, mapImageScaleX,
+                     mapImageScaleY
+
+    --]]
+
+    -- Initial camera's defaults and configs
+
+    args.screenWidth   = args.screenWidth   or love.graphics.getWidth()
+    args.screenHeight  = args.screenHeight  or love.graphics.getHeight()
+    args.screenHorizon = args.screenHorizon or args.screenHeight / 2
+
+    self.camera.screen.width   = args.screenWidth
+    self.camera.screen.height  = args.screenHeight
+    self.camera.screen.horizon = args.screenHorizon
+
+    -- Initial map's config and defaults
+
+    args.mapWidth       = args.mapWidth       or args.mapImageData:getWidth()
+    args.mapLength      = args.mapLength      or args.mapImageData:getHeight()
+    args.mapImageScaleX = args.mapImageScaleX or 1
+    args.mapImageScaleY = args.mapImageScaleY or 1
+
+    self.map.image.data    = args.mapImageData
+    self.map.width         = args.mapWidth
+    self.map.length        = args.mapLength
+    self.map.image.scale.x = args.mapImageScaleX
+    self.map.image.scale.y = args.mapImageScaleY
+
+    -- Autoconfigs
+
+    self.map.image.width   = args.mapImageData:getWidth()
+    self.map.image.height  = args.mapImageData:getHeight()
 
 end
 
+function self.camera.update(args)
 
+    --[[
 
+        Possible arguments:
 
+        - Position : positionX, positionY, positionZ,
+        - Screen   : screenWidth, screenHeight, screenHorizon, screenScanLines
+        - Others   : rotation, pitch
 
---------------------------------------------------------------------------------
+    --]]
 
+    -- Camera's position configs and defaults
 
+    args.positionX = args.positionX or self.camera.position.x
+    args.positionY = args.positionY or self.camera.position.y
+    args.positionZ = args.positionZ or self.camera.position.z
 
+    self.camera.position.x = args.positionX
+    self.camera.position.y = args.positionY
+    self.camera.position.z = args.positionZ
 
-
---------------------------------------------------------------------------------
-
-function self.camera.setPosition(x, y, z)
-
-    self.camera.position.x = x or self.camera.position.x
-    self.camera.position.y = y or self.camera.position.y
-    self.camera.position.z = z or self.camera.position.z
-
-    if self.camera.position.z <= 1 and self.enableGroundLimit then
+    if args.positionZ <= 1 and self.enableGroundLimit then
 
         self.camera.position.z = 1
 
     end
 
-end
+    -- Camera's other configs and defaults
 
-function self.camera.setSize(width, height)
+    args.rotation = args.rotation or self.camera.rotation
+    args.pitch    = args.pitch    or self.camera.pitch
 
-    self.camera.width = width or self.camera.width
-    self.camera.height = height or self.camera.height
+    self.camera.rotation        = args.rotation % 360
+    self.camera.pitch           = args.pitch
 
-end
+    -- Camera's screen configs and defaults
 
-function self.camera.setHorizon(height)
+    args.screenWidth     = args.screenWidth     or self.camera.screen.width
+    args.screenHeight    = args.screenHeight    or self.camera.screen.height
+    args.screenHorizon   = args.screenHorizon   or self.camera.screen.height / 2
+    args.screenScanLines = args.screenScanLines or 1
 
-    self.camera.horizon = height + self.camera.pitch
-
-end
-
-function self.camera.setRotation(angle)
-
-    self.camera.rotation = angle
-
-end
-
-function self.camera.setZoom(level)
-
-    self.camera.zoom = level
+    self.camera.screen.width     = args.screenWidth
+    self.camera.screen.height    = args.screenHeight
+    self.camera.screen.horizon   = args.screenHorizon + self.camera.pitch
+    self.camera.screen.scanLines = args.screenScanLines
 
 end
 
-function self.camera.setFov(fov)
+function self.map.update(args)
 
-    self.camera.fov = fov
+    --[[
 
-end
+        Possible arguments:
 
-function self.camera.setPitch(pitch)
+        - Position : positionX, positionY
+        - Size     : width, length
+        - Image    : imageData, imageWidth, imageHeight,
+                     imageScaleX, imageScaleY
 
-    self.camera.pitch = pitch
+    --]]
 
-end
+    -- Map's position configs and defaults
 
-function self.camera.setScanLines(linesCount)
+    args.positionX = args.positionX or self.map.position.x
+    args.positionY = args.positionY or self.map.position.y
 
-    self.camera.scanLines = linesCount
+    self.map.position.x = args.positionX
+    self.map.position.y = args.positionY
 
-end
+    -- Map's size configs and defaults
 
---------------------------------------------------------------------------------
+    args.width  = args.width  or self.map.width
+    args.length = args.length or self.map.length
 
+    self.map.width  = args.width
+    self.map.length = args.length
 
+    -- Map's image configs and defaults
 
+    args.imageData   = args.imageData   or self.map.image.data
+    args.imageWidth  = args.imageWidth  or self.map.image.width
+    args.imageHeight = args.imageHeight or self.map.image.height
+    args.imageScaleX = args.imageScaleX or self.map.image.scale.x
+    args.imageScaleY = args.imageScaleY or self.map.image.scale.y
 
-
-
-
-
-
-
-
---[[
-function self.newCamera(width, height, x, y, z, horizon,
-    rotation, zoom, fov, pitch, scanLines)
-
-    -- Property             Params      Defaults
-
-    self.camera.width       = width     or love.graphics.getWidth()
-    self.camera.height      = height    or love.graphics.getHeight()
-
-    self.camera.position.x  = x         or 0
-    self.camera.position.y  = y         or 0
-    self.camera.position.z  = z         or 0
-
-    self.camera.horizon     = horizon   or (love.graphics.getHeight() / 2)
-    self.camera.rotation    = rotation  or 0
-    self.camera.zoom        = zoom      or 1
-    self.camera.fov         = fov       or self.camera.horizon
-    self.camera.pitch       = pitch     or 0
-    self.camera.scanLines   = scanLines or 1
+    self.map.image.data    = args.imageData
+    self.map.image.width   = args.imageWidth
+    self.map.image.height  = args.imageHeight
+    self.map.image.scale.x = args.imageScaleX
+    self.map.image.scale.y = args.imageScaleY
 
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---]]
-
---------------------------------------------------------------------------------
---[[
-function self.init(_screenWidth, _screenHeight, _mapWidth, _mapLength, _imageData)
-
-    self.screenWidth    = _screenWidth
-    self.screenHeight   = _screenHeight
-
-    self.mapWidth       = _mapWidth
-    self.mapLength      = _mapLength
-
-    self.imageData      = _imageData
-    self.imageWidth     = _imageData:getWidth() - 1
-    self.imageHeight    = _imageData:getHeight() - 1
-
-    -- Usually you will want keep the screen
-    -- dimensions as the texture's scales values,
-    -- although you can adjust it.
-
-    self.textureXscale  = 1
-    self.textureYscale  = 1
-
-end
-
---------------------------------------------------------------------------------
--- @ Method         self.update()
--- @ Description    Update instance's camera variables. It must be called
---                  inside love.update() method.
---------------------------------------------------------------------------------
-
-function self.update(_positionX, _positionY, _positionZ, _rotationAngle,
-                     _zoom, _fov, _pitch, _horizon, _scanLines)
-
-    self.camera.positionX       = (_positionX + self.mapLength)
-    self.camera.positionY       = (_positionY + self.mapWidth)
-    self.camera.positionZ       = _positionZ
-    self.camera.rotationSin     = math.sin(math.rad(_rotationAngle))
-    self.camera.rotationCos     = math.cos(math.rad(_rotationAngle))
-    self.camera.zoom            = _zoom
-    self.camera.fov             = _fov
-    self.camera.pitch           = _pitch
-    self.camera.horizon         = _horizon + _pitch
-    self.camera.scanLines       = _scanLines
-
-end
---]]
---------------------------------------------------------------------------------
--- @ Method         self.draw()
--- @ Description    Do all the mode seven effect. It must be called
---                  inside love.draw() method.
---------------------------------------------------------------------------------
---[[
 function self.draw()
-
-    local x, y, z
-    local distance, ratio
-    local rx, ry, px, py, pixelX, pixelY
-
-    for y = self.camera.horizon, self.screenHeight, self.camera.scanLines do
-
-        z = y - self.camera.horizon
-
-        if z == 0 then z = z + 1 end
-
-        -- Tip: distance = (self.camera.positionZ * self.textureYscale) / z
-        --                   + self.camera.horizon
-        -- make a cool "earth flying" effect.
-        distance = (self.camera.positionZ * self.textureYscale) / z
-        ratio    = distance / self.textureXscale
-
-        rx = -self.camera.rotationSin * ratio
-        ry = self.camera.rotationCos * ratio
-
-        px = self.camera.positionX + distance * self.camera.rotationCos
-             * self.camera.zoom - self.screenWidth / 2 * rx
-
-        py = self.camera.positionY + distance * self.camera.rotationSin
-             * self.camera.zoom - self.screenWidth / 2 * ry
-
-        for x = 0, self.screenWidth, 1 do
-            if (math.abs(px * -1) < self.mapWidth)
-            and (math.abs(py * -1) < self.mapLength)
-            then
-                pixelX = math.floor(math.abs(px % self.imageWidth))
-                pixelY = math.floor(math.abs(py % self.imageHeight))
-
-                local r, g, b = self.imageData:getPixel(pixelX, pixelY)
-                love.graphics.setColor(r, g, b)
-
-                love.graphics.points(x, y)
-
-                px = px + rx
-                py = py + ry
-            end
-
-        end
-
-    end
-
-end
---]]
-
-function self.draw3(debugar)
 
     -- We want to start from image's bottom to horizon,
     -- decreasing after every loop. This get the effect's
     -- explanation a little friendly using the Z's gradient
     -- as an example.
-    --
-    -- NOTE: Changing the code to "for y = self.camera.horizon,
-    --                               self.screenHeight,
-    --                               self.camera.scanLines do"
-    -- And "projection.z = self.camera.horizon - y"
-    -- Will lead us to same results.
-    --
     -- This approach follows @tigrou's Mode 7 explanation below
     -- https://gamedev.stackexchange.com/a/28764
-
-    -- Working rotation:
-    -- position.x = (screenX + self.camera.width / 2) * rotation.cos - (screenX) * rotation.sin
-    -- position.y = fov * rotation.sin + (screenX) * rotation.cos
 
     local position = { x, y }
 
     local rotation = {
 
-        sin = math.sin(math.rad(self.camera.rotation)),
-        cos = math.cos(math.rad(self.camera.rotation))
+        sin = math.sin(math.rad(self.camera.rotation - 45)),
+        cos = math.cos(math.rad(self.camera.rotation - 45))
 
     }
 
     local projection = { x, y, z }
 
-    for screenY = self.camera.horizon, self.camera.height, self.camera.scanLines do
-
-        -- Adjusts
-
-        fov = self.camera.horizon
+    for screenY = self.camera.screen.horizon,
+                  self.camera.screen.height,
+                  self.camera.screen.scanLines
+                  do
 
         -- Z's projection
 
-        projection.z = self.camera.horizon - screenY
+        projection.z = screenY - self.camera.screen.horizon
         if projection.z == 0 then projection.z = projection.z + 1 end
 
-        for screenX = 0, self.camera.width, 1 do
+        for screenX = 0, self.camera.screen.width, 1 do
 
             -- X's and Y's positions
 
-            position.x = (self.camera.width - screenX)
-            position.y = (self.camera.width - screenX)
+            position.x = self.camera.screen.width - screenX
+            position.y = self.camera.screen.width - screenX
 
-            -- Adjusts
+            -- X's and Y's rotation
 
-            projection.x = (position.x) * rotation.sin + screenX * rotation.cos
-            projection.y = (position.y - self.camera.pitch) * rotation.cos - screenX * rotation.sin
+            projection.x = position.x * rotation.cos - screenX * rotation.sin
+            projection.y = position.y * rotation.sin + screenX * rotation.cos
 
             -- X's and Y's world projections
 
-            projection.x = projection.x * self.camera.position.z / projection.z
-            projection.y = projection.y * self.camera.position.z / projection.z
+            projection.x = projection.x / projection.z * self.camera.position.z
+            projection.y = projection.y / projection.z * self.camera.position.z
 
             -- Movement
 
             projection.x = projection.x + self.camera.position.x
             projection.y = projection.y + self.camera.position.y
 
-            -- Rotation
-
-            --projection.x = projection.x * rotation.cos - projection.y * rotation.sin
-            --projection.y = projection.x * rotation.sin + projection.y * rotation.cos
-
             -- Texture scaling
 
             projection.x = projection.x * self.map.image.scale.x
             projection.y = projection.y * self.map.image.scale.y
 
-            -- If the world's projected coordinate (projection.x, projection.y) are inside
-            -- maps range, we can display it on screen. This avoid the
-            -- "infinite map" effect.
+            -- We are going to draw the world's projected
+            -- coordinates (projection.x, projection.y) only
+            -- if they are inside map's range. With this,
+            -- we'll able to avoid the "infinite map" effect.
 
             if (projection.x > 0 and projection.x < self.map.width)
             and (projection.y > 0 and projection.y < self.map.length)
             then
 
-                -- Because (projection.x, projection.y) are actually world's coordinates, we need
-                -- to limit their range to the image's dimension's range.
+                -- Because (projection.x, projection.y) are actually
+                -- world's coordinates, we need to limit their range
+                -- to the image's dimension's range.
 
                 projection.x = projection.x % self.map.image.width
                 projection.y = projection.y % self.map.image.height
--- src/main.lua
+++ src/main.lua
@@ -1,28 +1,70 @@
---local __GLOBALCONFS = require "config/global"
---local graph         = require "lib/utils"
+--[[
+
+  @ File              main.lua
+  @ Path              /src/
+  @ Author            Brendo Costa <brendocosta@id.uff.br>
+  @ Description       Nintendo SNES's "mode seven" Love2d implementation
+  @ Updated           Nov 1, 2021
+
+  ------------------------------------------------------------------------------
+
+  MIT License
+
+  Copyright (c) 2021 Brendo Costa
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+--]]
+
+-- Global configuration
 
 local modeSevenLib = require "include/mode_seven"
-local graph = require "include/utils"
-local screenW = 800
-local screenH = 600
-local debugar = 1
+local windowWidth  = 800
+local windowHeight = 600
 
-player = {
+-- Set "enableRotationTest = true" for the auto rotating example
 
-    x = 0,
-    y = 0,
-    z = 10,
+local enableRotationTest = false
+local autorotationAngle  = 0
+
+-- Player / Controllable Camera
+-- With "x = 256" and "y = 256" we'll start at map's center,
+-- assuming a 512x512 map size.
+
+local player = {
+
+    x = 256,
+    y = 256,
+    z = 20,
     pitch = 0,
-    scanLines = 3,
-    angle = 180
+    angle = 0,
+    scanLines = 1
 
 }
 
-local mapFile = love.image.newImageData("assets/map_1.png")
-
 function love.load()
 
-	love.window.setMode(screenW, screenH, {
+    -- Window configuration
+
+    love.window.setTitle("Mode 7 Test")
+
+	love.window.setMode(windowWidth, windowHeight, {
 
         resizable	= false,
 		fullscreen	= false,
@@ -32,100 +74,144 @@ function love.load()
 
 	})
 
-    --love.window.setTitle("Floppalístico")
+    -- Mode 7 configuration
 
-	modeSevenLib.init(screenW, screenH, 512, 512, mapFile)
+    modeSevenLib.init({
+
+        screenWidth  = windowWidth,
+        screenHeight = windowHeight,
+        mapImageData = love.image.newImageData("assets/test.png"),
+        mapWidth     = 512,
+        mapLength    = 512
+
+    })
+
+    -- For auto rotating example
+
+    if enableRotationTest then
+
+        player.x = -120
+        player.y = -120
+        player.z = 120
+        player.pitch = -120
+
+    end
 
 end
 
 function love.update(dt)
 
-    -- Movimentos básicos
-
-	player.angle = player.angle % 360
-
-	--love.window.setTitle(player.angle % 400)
-    love.window.setTitle("X: " .. player.x .. " Y: " .. player.y .. " A: " .. player.angle)
+    -- Basic movement
 
     if love.keyboard.isDown("w") then
-        --player.x = player.x - (300 * math.cos(math.rad(player.angle))) * dt
-        --player.y = player.y - (300 * math.sin(math.rad(player.angle))) * dt
-        player.y = player.y + 100 * dt
+
+        player.x = player.x + (100 * math.cos(math.rad(player.angle))) * dt
+        player.y = player.y + (100 * math.sin(math.rad(player.angle))) * dt
+
 	end
 
     if love.keyboard.isDown("s") then
-        --player.x = player.x + (300 * math.cos(math.rad(player.angle))) * dt
-        --player.y = player.y + (300 * math.sin(math.rad(player.angle))) * dt
-        player.y = player.y - 100 * dt
+
+        player.x = player.x - (100 * math.cos(math.rad(player.angle))) * dt
+        player.y = player.y - (100 * math.sin(math.rad(player.angle))) * dt
+
 	end
 
 	if love.keyboard.isDown("a") then
-		--player.angle = player.angle - 100 * dt
-        player.x = player.x - 100 * dt
+
+		player.angle = player.angle - 100 * dt
+
 	end
 
     if love.keyboard.isDown("d") then
-        --player.angle = player.angle + 100 * dt
-        player.x = player.x + 100 * dt
+
+        player.angle = player.angle + 100 * dt
+
 	end
 
-    -- Altura Z
-
     if love.keyboard.isDown("lshift") then
+
 		player.z = player.z + 100 * dt
+
 	end
 
     if love.keyboard.isDown("lctrl") then
-		player.z = player.z - 100 * dt
+
+		if player.z > 10 then player.z = player.z - 100 * dt end
+
 	end
 
-    -- Angulo visão
-
-    if love.keyboard.isDown("q") then
-		player.angle = player.angle - 50 * dt
-	end
-
-    if love.keyboard.isDown("e") then
-		player.angle = player.angle + 50 * dt
-	end
-
-    -- FOV
+    -- View's pitch
 
     if love.keyboard.isDown("z") then
+
 		player.pitch = player.pitch + 100 * dt
-	end
-	if love.keyboard.isDown("x") then
-		player.pitch = player.pitch - 100 * dt
+
 	end
 
-    -- Scan Lines
+    if love.keyboard.isDown("x") then
+
+        player.pitch = player.pitch - 100 * dt
+
+	end
+
+    -- Renderer's scan lines
 
     if love.keyboard.isDown("c") then
+
 		player.scanLines = player.scanLines + 1
+
 	end
-	if love.keyboard.isDown("v") and player.scanLines > 1 then
+
+    if love.keyboard.isDown("v") and player.scanLines > 1 then
+
 		player.scanLines = player.scanLines - 1
+
 	end
 
-    if love.keyboard.isDown("f") then
-		debugar = debugar + 1 * dt
-	end
-	if love.keyboard.isDown("g") and player.scanLines > 1 then
-		debugar = debugar - 1 * dt
-	end
+    -- For autorotating example
 
-    modeSevenLib.camera.setPosition(player.x, player.y, player.z)
-    modeSevenLib.camera.setRotation(player.angle)
-    modeSevenLib.camera.setPitch(player.pitch)
-    modeSevenLib.camera.setHorizon(screenH/2)
-    modeSevenLib.camera.setScanLines(player.scanLines)
-    modeSevenLib.camera.setZoom(1)
+    if enableRotationTest then
+
+        -- Assuming the 512x512 map size
+
+        mapCenterX = 256
+        mapCenterY = 256
+
+        autorotationAngle = (autorotationAngle + 1 * ( (1 / 3600) % 1) * math.cos(mapCenterX) ) % 1
+
+        newX = math.cos(math.rad(autorotationAngle)) * (player.x - 256) - math.sin(math.rad(autorotationAngle)) * (player.y - 256) + 256
+        newY = math.sin(math.rad(autorotationAngle)) * (player.x - 256) + math.cos(math.rad(autorotationAngle)) * (player.y - 256) + 256
+
+        player.x = newX
+        player.y = newY
+
+        player.angle = ( math.atan2(256 - newY, 256 - newX) * (180 / math.pi) )
+
+    end
+
+    modeSevenLib.camera.update({
+
+        positionX = player.x,
+        positionY = player.y,
+        positionZ = player.z,
+        rotation  = player.angle,
+        pitch     = player.pitch,
+        screenScanLines = player.scanLines,
+
+    })
 
 end
 
 
 function love.draw()
 
-	modeSevenLib.draw3(debugar)
+    -- A blue background...
+
+    love.graphics.clear(155/256, 2411/256, 216/256)
+
+    -- Do the mode 7 !!
+
+    modeSevenLib.draw()
 
 end
